cd00c10802db24ac1c92841f230756cb60607c79
Max Marquart <maximilian.mqr@gmail.com>
2026-06-18 16:58:43 +0200

    Drop unnecessary verified parameter

--- Casks/dockintercept.rb
+++ Casks/dockintercept.rb
@@ -2,8 +2,7 @@ cask "dockintercept" do
   version "1.0.0"
   sha256 "c7be6683b9592875cc4931c34a2dd6426d942c0b2167e745e58fe28fc7f81beb"
 
-  url "https://github.com/MR-MANDELBROT/DockIntercept/releases/download/v#{version}/DockIntercept-#{version}.dmg",
-      verified: "github.com/MR-MANDELBROT/DockIntercept/"
+  url "https://github.com/MR-MANDELBROT/DockIntercept/releases/download/v#{version}/DockIntercept-#{version}.dmg"
   name "DockIntercept"
   desc "Extended right-click context menu for files and folders in the macOS Dock"
   homepage "https://github.com/MR-MANDELBROT/DockIntercept"
